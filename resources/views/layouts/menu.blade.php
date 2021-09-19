d7635e16d5f451728f7f8490cb3121a516346c54
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2021-09-19 08:10:24 +0100

    spatie permission and activity log packages added and installed to the project

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,28 +1,19 @@
-
 <li class="nav-item">
-    <a href="{{ route('equipment.index') }}"
-       class="nav-link {{ Request::is('equipment*') ? 'active' : '' }}">
+    <a href="{{ route('equipment.index') }}" class="nav-link {{ Request::is('equipment*') ? 'active' : '' }}">
         <p>Equipment</p>
     </a>
 </li>
 
 
 <li class="nav-item">
-    <a href="{{ route('states.index') }}"
-       class="nav-link {{ Request::is('states*') ? 'active' : '' }}">
+    <a href="{{ route('states.index') }}" class="nav-link {{ Request::is('states*') ? 'active' : '' }}">
         <p>States</p>
     </a>
 </li>
 
 
-
-
-
 <li class="nav-item">
-    <a href="{{ route('equipmentStates.index') }}"
-       class="nav-link {{ Request::is('equipmentStates*') ? 'active' : '' }}">
+    <a href="{{ route('equipmentStates.index') }}" class="nav-link {{ Request::is('equipmentStates*') ? 'active' : '' }}">
         <p>Equipment States</p>
     </a>
 </li>
-
-
